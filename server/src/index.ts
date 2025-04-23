a56204797bfe112505dfd796bc6bbaa1f8eac1c1
harrshitaSingh <harshit2000singh021@gmail.com>
2025-04-23 22:40:45 +0530

    Frontend URl Added

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: "https://your-frontend.netlify.app",
+  origin: "https://digitcv.netlify.app",
   credentials: true
 }));
 
