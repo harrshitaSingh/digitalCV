e84161bbcb15e027d66754fd1a601fef0c7d5928
harrshitaSingh <harshit2000singh021@gmail.com>
2025-04-23 22:52:45 +0530

    Frontend URL

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,20 +6,10 @@ import resumeRoutes  from "./Routes/resumeRoute";
 
 const prisma = new PrismaClient();
 const app = express();
-const allowedOrigins = ['https://digitcv.netlify.app', 'http://localhost:3000'];
-
-
-
 
 app.use(express.json());
 app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: "https://digitcv.netlify.app",
   credentials: true
 }));
 
