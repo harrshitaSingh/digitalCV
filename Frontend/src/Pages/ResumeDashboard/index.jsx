4c900f0be45b1483f9940ab21e2b1e62dc6a2f77
harrshitaSingh <harshit2000singh021@gmail.com>
2025-04-24 09:35:44 +0530

    Changes in Share Url

--- Frontend/src/Pages/ResumeDashboard/index.jsx
+++ Frontend/src/Pages/ResumeDashboard/index.jsx
@@ -423,7 +423,7 @@ function ResumeDashboard() {
         {shareResumeId && (
           <CustomModal isOpen={true} closeModal={() => setShareResumeId(null)}>
             <CustomShareButton
-              url={`http://localhost:3000/resume/${shareResumeId}`}
+              url={`http://digitcv.netlify.app/resume/${shareResumeId}`}
               resume={resumes.find((resume) => resume.id === shareResumeId)}
               onClose={() => setShareResumeId(null)}
             />
