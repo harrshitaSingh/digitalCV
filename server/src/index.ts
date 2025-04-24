ba2019574babe5c55eda2447e687b8d14c05484a
harrshitaSingh <harshit2000singh021@gmail.com>
2025-04-24 09:04:13 +0530

    Docker File updated

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,7 +14,7 @@ app.use(cors({
 }));
 
 app.use("/auth", authRoutes);
-app.use("/resume", resumeRoutes);
+app.use("/resume",  resumeRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
